0783ef28088a0b52889b1ba5e50edf1cb8ac7167
NawidT <nawidtahmid@gmail.com>
2024-03-01 00:32:27 -0500

    sick edits

--- src/App.js
+++ src/App.js
@@ -33,23 +33,23 @@ export default function App() {
         >  
           <ScaleLoader color="black" height="300px" width="3vw" />
         </Flex>}
-        <Flex direction="column" overflow="hidden" zIndex={2} height={videoLoaded ? "90vh": "0"} width="90%" alignItems="center" justifyContent="center"
+        <Flex direction="column" overflow="hidden" zIndex={2} height={videoLoaded ? "80vh": "0"} width="90%" alignItems="center" justifyContent="center"
           border="14px solid black" opacity={videoLoaded ? 1 : 0}>
 
-          <Flex direction="column" color="white" fontWeight={1000} fontSize="3rem" overflow="hidden" 
-            fontFamily="monospace" alignItems="center" justifyContent="center" >
-
-            <Text textAlign="center" bgColor="black" > 1INFINITY </Text>
-            <Flex direction="row" height="140px" >
-              <Image src={transparentlogo} width="100px" height="100px" position="relative" top="40px" opacity={videoLoaded ? 1 : 0}/>
-              <Text textAlign="center" position="relative" right="2vw" bgColor="black" > LOBAL </Text>
+          <Flex direction="column" color="white" fontWeight={1000} fontSize="3rem" fontFamily="monospace" alignItems="center" justifyContent="center" >
+            <Flex direction="row" justifyContent="center" alignItems="center" mt="5vh" >
+              <Image src={transparentlogo} sx={{filter: 'saturate(0%) grayscale(100%) brightness(1000%)'}} width="100px" height="100px" opacity={videoLoaded ? 1 : 0}/>
+              <Text color="white" position="relative" right="2vw" bottom="2vh" fontWeight={1200} > LOBAL </Text>
             </Flex>
+            <Text textAlign="center" bgColor="black" > 1INFINITY </Text>
             <Text textAlign="center" bgColor="black" > FUND </Text>
 
           </Flex>
+
+          <Text textAlign="center" color="white" fontWeight="light" fontStyle="italic" fontSize="3.5rem" > - early stage ai growth fund - </Text>
         </Flex>
 
-        <Box overflow="hidden" maxWidth="1560px" width="100%" zIndex={1} >
+        <Box overflow="hidden" maxWidth="1560px" width="100vw" zIndex={1} >
           <video style={{"object-fit": "cover", "width": "100vw", "height": "100vh", "position": "fixed", "top": "0", "left": "0"}} 
             width="100%" autoPlay loop muted playsInline ref={videoRef} >
             <source src={longbgvideo} type="video/mp4" />
