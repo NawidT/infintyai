c173d60b5708ef8864024f3e1d16cd9024ff5225
NawidT <nawidtahmid@gmail.com>
2024-03-01 21:37:07 -0500

    ui adjustments

--- src/App.js
+++ src/App.js
@@ -33,20 +33,20 @@ export default function App() {
         >  
           <ScaleLoader color="black" height="300px" width="3vw" />
         </Flex>}
-        <Flex direction="column" overflow="hidden" zIndex={2} height={videoLoaded ? "80vh": "0"} width="90%" alignItems="center" justifyContent="center"
-          border="14px solid black" opacity={videoLoaded ? 1 : 0}>
 
-          <Flex direction="column" color="white" fontWeight={1000} fontSize="3rem" fontFamily="monospace" alignItems="center" justifyContent="center" >
-            <Flex direction="row" justifyContent="center" alignItems="center" mt="5vh" >
-              <Image src={transparentlogo} sx={{filter: 'saturate(0%) grayscale(100%) brightness(1000%)'}} width="100px" height="100px" opacity={videoLoaded ? 1 : 0}/>
-              <Text color="white" position="relative" right="2vw" bottom="2vh" fontWeight={1200} > LOBAL </Text>
-            </Flex>
-            <Text textAlign="center" bgColor="black" > 1INFINITY </Text>
-            <Text textAlign="center" bgColor="black" > FUND </Text>
+        <Flex direction="column" overflow="hidden" zIndex={2} height={videoLoaded ? "95vh": "0"} width="90%" alignItems="center" justifyContent="space-between"
+           opacity={videoLoaded ? 1 : 0}>
 
+          <Flex direction="column" color="white" fontSize="3rem" fontFamily="monospace">
+              <Text textAlign="center" fontWeight="bold" > 1INFINITY VENTURES </Text>
+              <Flex direction="row" justifyContent="center" alignItems="center">
+                <Image src={transparentlogo} sx={{filter: 'saturate(0%) grayscale(100%) brightness(1000%)'}} width="100px" height="100px" opacity={videoLoaded ? 1 : 0}/>
+                <Text color="white" position="relative" right="2vw" bottom="2vh" fontWeight="bold" > LOBAL FUND </Text>
+              </Flex> 
           </Flex>
 
-          <Text textAlign="center" color="white" fontWeight="light" fontStyle="italic" fontSize="3.5rem" > - early stage ai growth fund - </Text>
+          <Text textAlign="center" color="violet" fontWeight="bold" fontStyle="italic" fontSize="3.5rem"
+          textShadow="1px 1px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000" > - coming soon - </Text>
         </Flex>
 
         <Box overflow="hidden" maxWidth="1560px" width="100vw" zIndex={1} >
